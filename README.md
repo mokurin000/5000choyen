22c58010d66cef4602ce1c9f5c8e4921267d6162
poly000 <1348292515@qq.com>
2022-09-06 04:02:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 5000choyen
+
+5000choyen generator wrapper for rust
+
+## Requirements
+
+- python >= 3.7
+    - pillow
+    - numpy
+
+- `res` in current directory
+
+## Credit
+
+[5000choyen](https://github.com/pcrbot/5000choyen)
+
+[pyO3](https://github.com/PyO3/pyo3)
+
+Google Noto Sans Fonts.
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use std::path::PathBuf;
+
+use pyo3::prelude::*;
+mod choyen_5000;
+
+pub fn generate_5000choyen(
+    top: &str,
+    bottom: &str,
+    file: PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
+    Python::with_gil(|py| {
+        let generator = PyModule::from_code(py, choyen_5000::CODE, "", "").unwrap();
+
+        generator
+            .call_method("genImage_to", (top, bottom, &file), None)
+            .unwrap();
+    });
+
+    Ok(())
+}
