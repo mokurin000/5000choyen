8bbfedf28e09ba9ba4029a6d2baaa8fdcd572288
poly000 <1348292515@qq.com>
2022-09-06 04:32:06 +0800

    a

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,13 @@ mod choyen_5000;
 pub fn generate_5000choyen(
     top: &str,
     bottom: &str,
-    file: PathBuf,
+    file: &PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
     Python::with_gil(|py| {
         let generator = PyModule::from_code(py, choyen_5000::CODE, "", "").unwrap();
 
         generator
-            .call_method("genImage_to", (top, bottom, &file), None)
+            .call_method("genImage_to", (top, bottom, file), None)
             .unwrap();
     });
 
